6cc1874c85b1c47e186687d96c428c282234ec74
jen <jendusseljee@gmail.com>
2023-11-30 16:19:12 +0100

    Update challenge 1

--- dailies/day1/sketch.js
+++ dailies/day1/sketch.js
@@ -1,8 +1,10 @@
 const CNV_WIDTH = 800;
 const CNV_HEIGHT = 800;
 
-let gridX = 10;
-let gridY = 10;
+let noiseScale = 0.3;
+
+let gridX = 15;
+let gridY = 15;
 
 let counter = 0;
 
@@ -11,7 +13,7 @@ function setup() {
 }
 
 function draw() {
-  background(220);
+  background(255);
   
   for (let x = 0; x < gridX; x++) {
     for (let y = 0; y < gridY; y++) {
@@ -24,15 +26,15 @@ function draw() {
 
 function drawShape(i, j) {
   noiseSeed(0);
-  const size = noise(i, j, counter/30) * 80;
-  const r = noise(i, j, counter/30 + 100) * 256;
-  const g = noise(i, j, counter/30 + 1000) * 256;
-  const b = noise(i, j, counter/30 + 10000) * 256;
+  const size = 10 + noise(i, j, counter/30) * 70;
+  const r = noise(i * noiseScale, j * noiseScale, counter/30 + 100) * 256;
+  const g = noise(i * noiseScale, j * noiseScale, counter/30 + 1000) * 256;
+  const b = noise(i * noiseScale, j * noiseScale, counter/30 + 10000) * 256;
   
   const x = i*(CNV_WIDTH/gridX) + ((CNV_WIDTH/gridX)/2 - size/2);
   const y = j*(CNV_HEIGHT/gridY) + ((CNV_HEIGHT/gridY)/2 - size/2);
   
-  const c = color(r, g, b);
+  const c = color(0, g, b);
   fill(c);
   noStroke();
 
